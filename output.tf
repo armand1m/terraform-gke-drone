313d866576732fe5324a5d9670456c4eea77ec56
Armando Magalhães <armando.mag95@gmail.com>
2020-01-02 01:04:13 +0100

    feat: add cloud dns outputs

--- output.tf
+++ output.tf
@@ -6,10 +6,26 @@ output "cluster_password" {
   value = google_container_cluster.ci.master_auth[0].password
 }
 
-output "endpoint" {
+output "cluster_endpoint" {
   value = google_container_cluster.ci.endpoint
 }
 
-output "node_pools" {
+output "cluster_node_pools" {
   value = google_container_cluster.ci.node_pool
+}
+
+output "cloud_dns_name" {
+  value = google_dns_managed_zone.main.dns_name
+}
+
+output "cloud_dns_name_servers" {
+  value = google_dns_managed_zone.main.name_servers
+}
+
+output "cloud_dns_example_record_set_name" {
+  value = google_dns_record_set.example.name
+}
+
+output "cloud_dns_example_record_set_rrdatas" {
+  value = google_dns_record_set.example.rrdatas
 }
